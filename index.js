125e82d36b3b523da60d14fe2abc45387cb50152
Michaeltrexex <mikhai273104@gmail.com>
2025-06-14 13:16:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const cookie = require('cookie');
 const PORT = 3000;
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root12',
-  database: 'todolist',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 async function retrieveListItems() {
